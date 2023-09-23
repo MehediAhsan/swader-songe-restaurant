1c9d0c3db587fa24bc8fa5c28acc53123f1e3b78
Mehedi Ahsan <mdmehedicse01@gmail.com>
2023-09-23 23:20:40 +0600

    setup router

---
+++ src/router/Router.jsx
@@ -0,0 +1,9 @@
+import { createBrowserRouter } from "react-router-dom";
+
+export const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <div className="text-black text-4xl">Hello world!</div>,
+    },
+  ]);
+  
