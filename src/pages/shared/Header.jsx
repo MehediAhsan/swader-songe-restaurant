3505356f590e9aca02fbeefc2b5d8029b4136909
Mehedi Ahsan <mdmehedicse01@gmail.com>
2023-09-24 11:20:29 +0600

    install react icons

--- src/pages/shared/Header.jsx
+++ src/pages/shared/Header.jsx
@@ -20,6 +20,8 @@ const Header = () => {
         },
 
     ]
+
+
     return (
         <div className="bg-black text-white">
             <div className="md:flex justify-between px-10 py-10">
@@ -27,6 +29,7 @@ const Header = () => {
                     <h1 className="text-2xl font-semibold">Swader Songe</h1>
                 </div>
                 <div>
+                    
                     <ul className="md:flex uppercase">
                         {
                             Menus.map(Menu => <li key={Menu.name} className="md:ml-6">
