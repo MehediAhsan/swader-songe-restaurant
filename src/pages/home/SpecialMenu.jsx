83c58449d1e321c650aadae59a36bb03a4cce21a
Mehedi Ahsan <mdmehedicse01@gmail.com>
2023-09-25 17:06:49 +0600

    design menucard

--- src/pages/home/SpecialMenu.jsx
+++ src/pages/home/SpecialMenu.jsx
@@ -1,14 +1,35 @@
 import SectionHeading from "../../components/SectionHeading";
+import { useState } from 'react';
+import { useEffect } from "react";
+import MenuCard from "../shared/MenuCard";
 
 
 const SpecialMenu = () => {
+    let [menu, setMenu] = useState([])
+
+    useEffect(() => {
+        fetch('menu.json')
+        .then(res => res.json())
+        .then (data => {
+            const specialMenu = data.filter(item => item.category === 'special')
+            setMenu(specialMenu)
+        })
+
+    },[])
     return (
         <div className="w-10/12 mx-auto">
             <SectionHeading 
             sub={"---Check it out---"}
             main={"our special menu"}
             ></SectionHeading>
-            
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+                {
+                    menu.map(item => <MenuCard
+                    key={item._id}
+                    item={item}
+                    ></MenuCard>)
+                }
+            </div>
         </div>
     );
 };
