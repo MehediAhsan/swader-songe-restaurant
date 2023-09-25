e317abde80bcfd9f14811f9141f9240f9e92c11b
Mehedi Ahsan <mdmehedicse01@gmail.com>
2023-09-25 12:41:57 +0600

    added about section

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,3 +1,4 @@
+import AboutSwaderSonge from "./AboutSwaderSonge";
 import Banner from "./Banner";
 import Categories from "./Categories/Categories";
 
@@ -6,6 +7,7 @@ const Home = () => {
         <div>
             <Banner></Banner>
             <Categories></Categories>
+            <AboutSwaderSonge></AboutSwaderSonge>
         </div>
     );
 };
