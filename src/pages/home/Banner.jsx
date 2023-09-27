3d6b0fea42dc43a38aaedda1c09593656be9e022
Mehedi Ahsan <mdmehedicse01@gmail.com>
2023-09-27 19:49:33 +0600

    style the banner

--- src/pages/home/Banner.jsx
+++ src/pages/home/Banner.jsx
@@ -1,6 +1,7 @@
 import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css"; 
 import '../../App.css'
+import { FaPhone, FaFacebook, FaYoutube, FaInstagram, FaTwitter, FaRegArrowAltCircleRight } from 'react-icons/fa';
 
 const Banner = () => {
     const slides = [
@@ -22,7 +23,7 @@ const Banner = () => {
         }
     ]
     return (
-        <div className="w-full">
+        <div className="w-full relative">
             <Carousel showArrows={true}
       infiniteLoop={true}
       showThumbs={true}
@@ -37,9 +38,28 @@ const Banner = () => {
       >
                 {
                     slides.map(slide => 
-                        <div key={slide.name}>
+                        <div key={slide.name} className="">
                             <img src={slide.img} className="h-screen" />
                             {/* <p className="legend">{slide.name}</p> */}
+                            <div className="absolute inset-0 bg-black opacity-40"></div>
+                            <div className="absolute inset-0 z-1 w-1/2 mx-auto flex flex-col gap-6 justify-center items-center">
+                                <h1 className="text-4xl font-semibold text-white font-serif">Wellcome to Swader Songe</h1>
+                            <button className="flex justify-center items-center gap-1 inset-0 text-xl px-5 py-1 font-semibold text-white border-2 border-orange-500 rounded-full">
+                                <span>Explore</span>
+                                <FaRegArrowAltCircleRight className="text-2xl "></FaRegArrowAltCircleRight>
+                            </button>
+                            <div className="flex justify-center items-center gap-2 text-white text-2xl">
+                                <FaPhone></FaPhone>
+                            <p className="">1246-5568-1325</p>
+                            </div>
+                            <div className="flex justify-center items-center gap-2 text-white text-2xl">
+                                <span>Follow Us:</span>
+                                <FaFacebook></FaFacebook>
+                                <FaInstagram></FaInstagram>
+                                <FaYoutube></FaYoutube>
+                                <FaTwitter></FaTwitter>
+                            </div>
+                            </div>
                         </div>
                     )
                 }
