90a033de96e419e970c39a137b195704567b917b
Mehedi Ahsan <mdmehedicse01@gmail.com>
2023-09-27 18:53:29 +0600

    set logo on navbar

--- src/pages/shared/Header.jsx
+++ src/pages/shared/Header.jsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 import {FaBars, FaTimes} from 'react-icons/fa'
 import { useState } from "react";
-import { FaHome, FaPhone, FaUtensils, FaShoppingCart } from "react-icons/fa";
+import { FaHome, FaEnvelope, FaUtensils, FaShoppingCart, FaTools } from "react-icons/fa";
 
 const Header = () => {
     const Menus = [
@@ -13,7 +13,7 @@ const Header = () => {
         {
             name: 'CONTACT US', 
             link:'/',
-            icon: <FaPhone></FaPhone>
+            icon: <FaEnvelope></FaEnvelope>
         },
         {
             name: 'Our Menu', 
@@ -31,13 +31,14 @@ const Header = () => {
     let [open, setOpen] = useState(false)
 
     return (
-        <div className="bg-black text-white w-full fixed top-0 left-0 z-50 opacity-90">
-            <div className="md:flex items-center justify-between px-14 py-8">
-                <div>
-                    <h1 className="text-2xl font-semibold">Swader Songe</h1>
+        <div className="bg-black text-white w-full fixed top-0 left-0 z-50 opacity-90 shadow-xl">
+            <div className="flex items-center justify-between px-14 py-7">
+                <div className="flex justify-center items-center text-2xl font-semibold gap-3">
+                    <FaTools className="text-orange-500 text-3xl"></FaTools>
+                    <h1 className="font-semibold font-sans italic">Swader Songe</h1>
                 </div>
                 <div>
-                    <div onClick={() => setOpen(!open)} className="md:hidden absolute top-10 right-10 text-xl">
+                    <div onClick={() => setOpen(!open)} className="md:hidden absolute top-8 right-10 text-xl">
                         {
                             open ? <FaTimes></FaTimes> : <FaBars></FaBars> 
                         }
@@ -45,8 +46,8 @@ const Header = () => {
                     
                     <ul className={`md:flex md:items-center justify-center absolute md:static bg-black md:z-auto z-[-1] left-0 w-full md:w-auto pl-9 md:pl-0 py-4 md:py-0 uppercase ${open ? 'top-20 opacity-100': 'top-[-490px] md:opacity-100 opacity-0'} `}>
                         {
-                            Menus.map(Menu => <li key={Menu.name} className="flex justify-center items-center gap-1 md:ml-6 my-6 md:my-0 hover:text-orange-400">
-                                <span className="text-xl">{Menu.icon}</span>
+                            Menus.map(Menu => <li key={Menu.name} className="flex justify-center items-center gap-1 md:ml-6 my-6 md:my-0 hover:text-orange-500 font-medium">
+                                <span className="text-xl text-orange-500">{Menu.icon}</span>
                                <Link to={Menu.link}>{Menu.name}</Link> 
                             </li>)
                         }
