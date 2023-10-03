1309b0ff419812b1bcaac98e817eabee2bfad3e3
Mehedi Ahsan <mdmehedicse01@gmail.com>
2023-10-03 20:55:09 +0600

    added tabs

--- src/pages/shop/Shop.jsx
+++ src/pages/shop/Shop.jsx
@@ -3,6 +3,7 @@ import { Helmet } from 'react-helmet-async';
 import SectionBox from './../shared/SectionBox';
 
 const Shop = () => {
+    const items = ['Buger', 'Chicken Chap', 'Pizza', 'Soups']
     return (
         <div>
             <Helmet>
@@ -11,6 +12,16 @@ const Shop = () => {
                 </title>
             </Helmet>
             <SectionBox title={"our shop"}></SectionBox>
+
+            <div className='my-20'>
+                <ul className='flex justify-center items-center gap-8 uppercase'>
+                    {
+                        items.map((item, index) => <li className='text-xl font-bold transition duration-300 hover:text-orange-500 cursor-pointer hover:border-b-4 hover:border-orange-500' key={index}>
+                            {item}
+                        </li>)
+                    }
+                </ul>
+            </div>
         </div>
     );
 };
