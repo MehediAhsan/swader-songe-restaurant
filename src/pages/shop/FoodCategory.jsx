28017d2046d13334cad98a4ba7376d745e71ab14
Mehedi Ahsan <mdmehedicse01@gmail.com>
2023-10-03 21:41:01 +0600

    added category food

---
+++ src/pages/shop/FoodCategory.jsx
@@ -0,0 +1,29 @@
+import { useEffect } from 'react';
+import { useState } from 'react';
+import FoodCard from './../shared/FoodCard';
+
+const FoodCategory = ({selectedFood}) => {
+    let [foods, setFoods] = useState([])
+
+    useEffect(() => {
+        fetch('menu.json')
+        .then(res => res.json())
+        .then (data => {
+            const specialFoods = data.filter(item => item.category === `${selectedFood}`)
+            setFoods(specialFoods)
+        })
+
+    },[selectedFood])
+
+    return (
+        <div className="w-10/12 mx-auto mb-20">
+           <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
+                {
+                    foods.map(food => <FoodCard key={food._id} food={food}></FoodCard>)
+                }
+            </div>
+        </div>
+    );
+};
+
+export default FoodCategory;
--- src/pages/shop/Shop.jsx
+++ src/pages/shop/Shop.jsx
@@ -1,9 +1,14 @@
 
 import { Helmet } from 'react-helmet-async';
 import SectionBox from './../shared/SectionBox';
+import { useState } from 'react';
+import FoodCategory from './FoodCategory';
 
 const Shop = () => {
-    const items = ['Buger', 'Chicken Chap', 'Pizza', 'Soups']
+    const items = ['burger', 'Chicken Chap', 'pizza', 'soup']
+    const [selectedFood, setSelectedFood] = useState('burger');
+
+
     return (
         <div>
             <Helmet>
@@ -16,12 +21,15 @@ const Shop = () => {
             <div className='my-20'>
                 <ul className='flex justify-center items-center gap-8 uppercase'>
                     {
-                        items.map((item, index) => <li className='text-xl font-bold transition duration-300 hover:text-orange-500 cursor-pointer hover:border-b-4 hover:border-orange-500' key={index}>
+                        items.map((item, index) => <li className={`text-xl font-bold transition duration-300 hover:text-orange-500 cursor-pointer hover:border-b-4 hover:border-orange-500 ${selectedFood === item? 'text-orange-500 border-b-4 border-orange-500': ''}`} onClick={() => setSelectedFood(item)} key={index}>
                             {item}
                         </li>)
                     }
                 </ul>
             </div>
+
+            <FoodCategory selectedFood={selectedFood}></FoodCategory>        
+
         </div>
     );
 };
