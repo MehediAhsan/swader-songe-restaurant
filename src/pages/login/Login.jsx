6f8d6a090203662fb4cc6d43e90bef33a3bfa883
Mehedi Ahsan <mdmehedicse01@gmail.com>
2023-10-07 23:26:14 +0600

    design login page

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -7,8 +7,20 @@ const Login = () => {
             <div className='w-1/2'>
                 <Lottie animationData={loginImg} />
             </div>
-            <div>
-                dfg
+            <div className='w-1/2 my-8'>
+                <h1 className='text-center text-2xl font-semibold my-4'>Login</h1>
+                <form action="" className='w-7/12 mx-auto flex flex-col gap-5'>
+                    <div>
+                        <legend className='text-lg font-medium mb-2'>Email</legend>
+                        <input type="email" name="" id="" className='p-3 w-full rounded' placeholder='Write your email...' />
+                    </div>
+                    <div>
+                        <legend className='text-lg font-medium mb-2'>Password</legend>
+                        <input type="password" name="" id="" className='p-3 w-full rounded' placeholder='Write your password...' />
+                    </div>
+                    <button className='bg-orange-400 p-3 rounded text-white font-medium'>Sign In</button>
+                    <p className='text-orange-400 text-center'>New here? Create a New Account</p>
+                </form>
             </div>
         </div>
     );
