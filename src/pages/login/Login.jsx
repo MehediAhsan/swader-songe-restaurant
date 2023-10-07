6bfcb8b19afd9ee80979137da4fc127f38e4c9e9
Mehedi Ahsan <mdmehedicse01@gmail.com>
2023-10-07 22:59:11 +0600

    added lottie animation

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,8 +1,15 @@
+import loginImg from '../../assets/login.json'
+import Lottie from "lottie-react";
 
 const Login = () => {
     return (
-        <div>
-            sdf
+        <div className="bg-gray-100 w-10/12 mx-auto my-44 flex">
+            <div className='w-1/2'>
+                <Lottie animationData={loginImg} />
+            </div>
+            <div>
+                dfg
+            </div>
         </div>
     );
 };
