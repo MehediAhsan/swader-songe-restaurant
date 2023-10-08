b9a9a9d6e85c5138bf0410e9c0f08783a980e0b8
Mehedi Ahsan <mdmehedicse01@gmail.com>
2023-10-08 17:48:31 +0600

    added sign up page

--- src/pages/register/SignUp.jsx
+++ src/pages/register/SignUp.jsx
@@ -3,14 +3,14 @@ import Lottie from "lottie-react";
 import {FaGoogle, FaFacebook, FaGithub} from 'react-icons/fa'
 import { Link } from 'react-router-dom';
 
-const Login = () => {
+const SignUp = () => {
     return (
-        <div className="bg-gray-100 w-10/12 mx-auto my-44 flex">
+        <div className="bg-gray-100 w-10/12 mx-auto my-44 flex flex-row-reverse">
             <div className='w-1/2'>
                 <Lottie animationData={loginImg} />
             </div>
             <div className='w-1/2 my-8'>
-                <h1 className='text-center text-2xl font-semibold my-4'>Login</h1>
+                <h1 className='text-center text-2xl font-semibold my-4'>SignUp</h1>
                 <form action="" className='w-7/12 mx-auto flex flex-col gap-5'>
                     <div>
                         <legend className='text-lg font-medium mb-2'>Email</legend>
@@ -20,8 +20,8 @@ const Login = () => {
                         <legend className='text-lg font-medium mb-2'>Password</legend>
                         <input type="password" name="" id="" className='p-3 w-full rounded border' placeholder='Write your password...' />
                     </div>
-                    <button className='bg-orange-400 p-3 rounded text-white font-medium'>Sign In</button>
-                    <Link to="/signup"><p className='text-orange-400 text-center'>New here? Create a New Account</p></Link>
+                    <button className='bg-orange-400 p-3 rounded text-white font-medium'>Sign Up</button>
+                    <Link to="/login"><p className='text-orange-400 text-center'>Already registered? Go to log in</p></Link>
                     <p className='text-gray-800 text-center'>Or sign in with</p>
                     <div className='flex justify-center items-center gap-10 text-4xl'>
                         <FaFacebook></FaFacebook>
@@ -34,4 +34,4 @@ const Login = () => {
     );
 };
 
-export default Login;
+export default SignUp;
--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -5,6 +5,7 @@ import ErrorPage from "../pages/error-page/ErrorPage";
 import Home from "../pages/home/Home";
 import Login from "../pages/login/Login";
 import Menu from "../pages/menu/Menu";
+import SignUp from "../pages/register/SignUp";
 import Shop from "../pages/shop/Shop";
 
 export const router = createBrowserRouter([
@@ -32,6 +33,10 @@ export const router = createBrowserRouter([
         {
           path: '/login',
           element: <Login></Login>
+        },
+        {
+          path: '/signup',
+          element: <SignUp></SignUp>
         }
       ]
     },
