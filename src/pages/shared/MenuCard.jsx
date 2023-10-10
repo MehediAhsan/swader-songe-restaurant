cc3c54aa9b5a6f0852f657beff036b5719a501de
Mehedi Ahsan <mdmehedicse01@gmail.com>
2023-10-10 21:45:13 +0600

    update json

--- src/pages/shared/MenuCard.jsx
+++ src/pages/shared/MenuCard.jsx
@@ -3,8 +3,8 @@
 const MenuCard = ({item}) => {
     const {image, name, description, price} = item;
     return (
-        <div className="flex flex-col md:flex-row space-y-2 md:space-x-2 shadow-md rounded p-2">
-            <img style={{borderRadius:"0px 200px 200px 200px"}} className="w-[118px] h-[104px] bg-gray-400 p-1" src={image} alt="" />
+        <div className="flex flex-col md:flex-row space-y-4 md:space-x-4 shadow-md rounded p-6 bg-orange-100 cursor-pointer border-2">
+            <img style={{borderRadius:"0px 200px 200px 200px"}} className="w-[118px] h-[104px] bg-gray-300 p-1" src={image} alt="" />
             <div>
                 <h1 className="text-xl font-medium">{name}</h1>
                 <p className="mt-1 text-gray-700">{description}</p>
