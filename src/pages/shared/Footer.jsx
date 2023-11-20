8b7624347f219dd754523b76d55227eb132a20c7
Mehedi Ahsan <mdmehedicse01@gmail.com>
2023-11-20 22:57:39 +0600

    change favicon

--- src/pages/shared/Footer.jsx
+++ src/pages/shared/Footer.jsx
@@ -3,7 +3,7 @@ const Footer = () => {
     return (
         <div>
             <div>
-                <p className="text-center text-xl font-semibold bg-black text-white py-4">Copyright © Swader Songe. All rights reserved.</p>
+                <p className="text-center bg-black opacity-95 text-white py-4"> © <span className="text-orange-500">Swader Songe.</span> All rights reserved.</p>
             </div>
         </div>
     );
