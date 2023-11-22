d1a2aafc97be89bdca9c93b25af9ca5a856312b6
Mehedi Ahsan <mdmehedicse01@gmail.com>
2023-11-22 19:39:42 +0600

    link setup

--- src/pages/shared/SideBar.jsx
+++ src/pages/shared/SideBar.jsx
@@ -2,6 +2,16 @@ import { FaThLarge } from "react-icons/fa";
 import { Link } from "react-router-dom";
 
 const SideBar = () => {
+  const menu = [
+    {
+      name: "menu",
+      link: "/"
+    },
+    {
+      name: "item",
+      link: "/"
+    }
+  ]
   return (
     <div className="bg-gray-200 w-72 rounded shadow">
       <div className="flex justify-center items-center text-xl font-semibold gap-2 bg-orange-200 rounded p-2">
@@ -10,6 +20,11 @@ const SideBar = () => {
           <h1 className="font-semibold font-sans uppercase">Dashboard</h1>
         </Link>
       </div>
+      {
+        menu.map((item, index) =>(
+          <Link to={item.link} key={index}>{item.name}</Link>
+        ))
+      }
     </div>
   );
 };
