ff7b0c1eb5370bf04795310e58f47434c8439edb
Mehedi Ahsan <mdmehedicse01@gmail.com>
2023-11-23 22:03:51 +0600

    design side menu

--- src/pages/shared/SideBar.jsx
+++ src/pages/shared/SideBar.jsx
@@ -1,30 +1,45 @@
-import { FaThLarge } from "react-icons/fa";
+import { FaThLarge, FaUser, FaCog, FaChartBar } from "react-icons/fa";
 import { Link } from "react-router-dom";
 
 const SideBar = () => {
   const menu = [
     {
-      name: "menu",
+      name: "Dashboard",
       link: "/"
     },
     {
-      name: "item",
-      link: "/"
+      name: "Profile",
+      link: "/profile"
+    },
+    {
+      name: "Settings",
+      link: "/settings"
+    },
+    {
+      name: "Analytics",
+      link: "/analytics"
     }
-  ]
+  ];
+
   return (
     <div className="bg-gray-200 w-72 rounded shadow">
       <div className="flex justify-center items-center text-xl font-semibold gap-2 bg-orange-200 rounded p-2">
-        <FaThLarge className="text-black text-xl"></FaThLarge>
+        <FaThLarge className="text-black text-xl" />
         <Link to="/">
           <h1 className="font-semibold font-sans uppercase">Dashboard</h1>
         </Link>
       </div>
-      {
-        menu.map((item, index) =>(
-          <Link to={item.link} key={index}>{item.name}</Link>
-        ))
-      }
+      <div className="p-4">
+        {menu.map((item, index) => (
+          <Link
+            to={item.link}
+            key={index}
+            className="block py-2 px-4 text-gray-700 hover:bg-gray-300 hover:text-black rounded transition duration-300"
+          >
+            {item.name}
+          </Link>
+        ))}
+      </div>
     </div>
   );
 };
