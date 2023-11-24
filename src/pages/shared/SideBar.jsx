b94714fc4bb7d27cc7dd06b0fd6d258adeb06ed9
Mehedi Ahsan <mdmehedicse01@gmail.com>
2023-11-24 23:15:15 +0600

    changed link

--- src/pages/shared/SideBar.jsx
+++ src/pages/shared/SideBar.jsx
@@ -39,7 +39,7 @@ const SideBar = () => {
     <div className="bg-gray-200 w-72 rounded shadow">
       <div className="flex justify-center items-center text-xl font-semibold gap-2 bg-orange-200 rounded p-2">
         <FaThLarge className="text-black text-xl" />
-        <Link to="/">
+        <Link to="/dashboard">
           <h1 className="font-semibold font-sans uppercase">Dashboard</h1>
         </Link>
       </div>
