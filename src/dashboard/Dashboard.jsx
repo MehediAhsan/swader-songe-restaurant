49f330720627bb5692631d874ba2813444fc074b
Mehedi Ahsan <mdmehedicse01@gmail.com>
2023-11-29 21:50:43 +0600

    design dashboard

--- src/dashboard/Dashboard.jsx
+++ src/dashboard/Dashboard.jsx
@@ -26,28 +26,28 @@ const Dashboard = () => {
             >
               <div className="text-gray-700 text-center">
                 <h1 className="text-3xl font-bold">{activity.timestamp}</h1>
-                <p className="text-2xl font-semibold">{activity.action}dsa</p>
+                <p className="text-2xl font-semibold">{activity.action}</p>
               </div>
             </div>
           );
         })}
       </div>
 
-      <div className="grid grid-cols-2">
-        <div className="flex flex-col gap-6 justify-center items-center">
+      <div className="grid grid-cols-2 my-10 divide-x-4 divide-primary">
+        <div className="flex flex-col gap-4 justify-center items-center bg-pink-100 p-16">
           <img
-            className="h-52 w-52 rounded-full"
+            className="h-40 w-40 rounded-full"
             src="https://avatars.githubusercontent.com/u/83613654?v=4"
             alt=""
           />
           <h1 className="text-2xl font-semibold">Md Mehedi Ahsan</h1>
         </div>
-        <div className="flex flex-col justify-center items-center gap-4">
+        <div className="flex flex-col justify-center items-center gap-4 bg-yellow-100 text-gray-800">
           <h1 className="text-2xl font-semibold">My Activities</h1>
-          <div className="text-2xl font-semibold flex flex-col gap-2">
+          <div className="text-xl font-semibold flex flex-col gap-2">
             <span>Oders: 66</span>
-            <span>Oders: 66</span>
-            <span>Oders: 66</span>
+            <span>Cart: 66</span>
+            <span>Payment: 66</span>
           </div>
         </div>
       </div>
