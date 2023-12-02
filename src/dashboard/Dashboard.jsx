cbd408dac56ee9f7230bdc32be2b546e21de9ce0
Mehedi Ahsan <mdmehedicse01@gmail.com>
2023-12-02 22:47:30 +0600

    base 64

--- src/dashboard/Dashboard.jsx
+++ src/dashboard/Dashboard.jsx
@@ -1,4 +1,7 @@
+import { useState } from 'react';
+
 const Dashboard = () => {
+
   const activities = [
     {
       timestamp: "200",
@@ -13,6 +16,26 @@ const Dashboard = () => {
       action: "New tasks",
     },
   ];
+
+  const [image, setImage] = useState(null);
+  const [preview, setPreview] = useState('');
+
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
+
+    if (file) {
+      // Read the file as a data URL
+      const reader = new FileReader();
+
+      reader.onloadend = () => {
+        // Set the preview and the base64-encoded image
+        setPreview(reader.result);
+        setImage(btoa(reader.result));
+      };
+
+      reader.readAsBinaryString(file);
+    }
+  };
 
   return (
     <div className="bg-gray-100 rounded shadow w-full p-10">
@@ -37,11 +60,19 @@ const Dashboard = () => {
         <div className="flex flex-col gap-4 justify-center items-center bg-pink-100 p-16">
           <img
             className="h-40 w-40 rounded-full"
-            src="https://avatars.githubusercontent.com/u/83613654?v=4"
+            src={preview}
             alt=""
           />
           <h1 className="text-2xl font-semibold">Md Mehedi Ahsan</h1>
+          <input type="file" accept="image/*" onChange={handleImageChange} />
+          {image && (
+        <div>
+          <h3>Base64 Encoded Image</h3>
+          <img src={image} alt="Encoded Image" style={{ maxWidth: '100%', maxHeight: '200px' }} />
         </div>
+      )}
+        </div>
+
         <div className="flex flex-col justify-center items-center gap-4 bg-yellow-100 text-gray-800">
           <h1 className="text-2xl font-semibold">My Activities</h1>
           <div className="text-xl font-semibold flex flex-col gap-2">
